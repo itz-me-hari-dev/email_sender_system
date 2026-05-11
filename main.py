5fd93480f3f0b540338f3c0205952116ccce0a5b
Harikrishnan M B <harikrishnan.mb.dev@gmail.com>
2026-05-11 22:03:07 +0530

    Remove unnecessary blank lines for improved code clarity

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from email_sender import run_sequential, run_threaded
 from db import init_db, clear_table
 
 
-
-
-
 # Configuration
 DEFAULT_MODE = "mailgun"   # "mock" or "mailgun"
 MAILGUN_TEST_FILE = "mailgun_test.csv"
